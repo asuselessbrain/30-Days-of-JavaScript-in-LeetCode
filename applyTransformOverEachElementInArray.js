cec18fd2cb8fd026227aa4d8599985137c88e3d4
asuselessbrain <shovon1023456789@gmail.com>
2024-08-25 15:25:16 +0600

    problem 2635 solved

---
+++ applyTransformOverEachElementInArray.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function (arr, fn) {
+  let result = [];
+
+  for (i = 0; i < arr.length; i++) {
+    result.push(fn(arr[i], i));
+  }
+  return result;
+};
+
+var map = function (fn, arr) {
+  let result = [];
+
+  for (i = 0; i < arr.length; i++) {
+    result.push(fn(arr[i], i));
+  }
+  return result;
+};
